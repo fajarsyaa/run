523a6349aa78931ae5555bcb9c9c91efd981de66
fajarsyaa <fajarsyaa@gmail.com>
2026-08-19 22:24:22 +0700

    first commit

---
+++ participants.js
@@ -0,0 +1,59 @@
+const participantsData = {
+  "1011": {
+    "name": "ADITYA ANGGARA",
+    "bib": "1011",
+    "distance": "10K",
+    "date": "5 July 2026",
+    "gender": "Male",
+    "category": "UMUM",
+    "status": "Finished",
+    "finishTime": "00:35:44",
+    "overallRank": "1/194",
+    "categoryRank": "1/155",
+    "genderRank": "1/175",
+    "certificatePdf": "cert.pdf",
+    "splits": [
+      { "name": "START", "time": "00:00:00", "timeOfDay": "(05:38:57)", "splitTime": "" },
+      { "name": "CP1", "time": "00:30:04", "timeOfDay": "(06:09:01)", "splitTime": "00:30:04" },
+      { "name": "Finish", "time": "00:35:44", "timeOfDay": "(06:14:41)", "splitTime": "00:05:39" }
+    ]
+  },
+  "1221": {
+    "name": "MUHAMMAD FAJAR SYAIHU WALID",
+    "bib": "1221",
+    "distance": "10K",
+    "date": "5 July 2026",
+    "gender": "Male",
+    "category": "UMUM",
+    "status": "Finished",
+    "finishTime": "00:58:02",
+    "overallRank": "62/194",
+    "categoryRank": "47/155",
+    "genderRank": "55/175",
+    "certificatePdf": "cert.pdf",
+    "splits": [
+      { "name": "START", "time": "00:00:00", "timeOfDay": "(05:38:57)", "splitTime": "" },
+      { "name": "CP1", "time": "00:50:10", "timeOfDay": "(06:29:07)", "splitTime": "00:50:10" },
+      { "name": "Finish", "time": "00:58:02", "timeOfDay": "(06:36:59)", "splitTime": "00:07:51" }
+    ]
+  },
+  "1222": {
+    "name": "HIDAYAT NUR WAHYUNING GUSTI",
+    "bib": "1222",
+    "distance": "10K",
+    "date": "5 July 2026",
+    "gender": "Male",
+    "category": "UMUM",
+    "status": "Finished",
+    "finishTime": "00:59:02",
+    "overallRank": "65/194",
+    "categoryRank": "50/155",
+    "genderRank": "58/175",
+    "certificatePdf": "cert.pdf",
+    "splits": [
+      { "name": "START", "time": "00:00:00", "timeOfDay": "(05:38:57)", "splitTime": "" },
+      { "name": "CP1", "time": "00:51:10", "timeOfDay": "(06:30:07)", "splitTime": "00:51:10" },
+      { "name": "Finish", "time": "00:59:02", "timeOfDay": "(06:37:59)", "splitTime": "00:07:52" }
+    ]
+  }
+};
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static assets from root folder
+app.use(express.static(__dirname));
+
+// Resolve relative assets inside /app/ path without modifying HTML
+app.use('/app', express.static(__dirname));
+
+// Details page
+app.get('/details', (req, res) => {
+  res.sendFile(path.join(__dirname, 'details.html'));
+});
+
+// Search / results with optional ?q= param
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Serve index.html for clean BIB-based URL paths
+app.get(['/app/:id', '/:id'], (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Default root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`  Results : http://localhost:${PORT}/`);
+  console.log(`  Details : http://localhost:${PORT}/details`);
+  console.log(`  Runner  : http://localhost:${PORT}/app/1011`);
+});
+
